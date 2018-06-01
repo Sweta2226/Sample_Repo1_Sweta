cb90cc2acd965963eb52a9b89c5031a0e0e1b122
tarkeshwarnath8649 <tarkeshwarnath8649@gmail.com>
2018-06-01 21:14:10 +0530

    Added new code

---
+++ src/test/java/mavenselenium/FirstTest.java
@@ -0,0 +1,15 @@
+package mavenselenium;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void firstTest1()
+	{
+		System.out.println("firstTest1");
+	}
+
+	
+
+}
---
+++ src/test/java/mavenselenium/SecondTest.java
@@ -0,0 +1,18 @@
+package mavenselenium;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	
+	@Test
+	public void secondTest1()
+	{
+		System.out.println("secondTest1");
+	}
+	@Test
+	public void secondTest2()
+	{
+		System.out.println("secondTest2");
+	}
+
+}
